97f52aea50325ddd34cac92df0be16c6dcf13b44
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-02-04 17:43:02 +0100

    Eslint file updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,17 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   root: true,
+  overrides: [
+    {
+      files: ['*.js'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+  ],
 };
